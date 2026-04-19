4892e6c3dc59f377a1c931f22628a07427c23c29
Abraham - CIBERSEGURIDAD ◇ HACKING <proabraham888@gmail.com>
2026-04-19 15:18:47 -0600

    Add global configuration settings in setting.js

---
+++ setting.js
@@ -0,0 +1,19 @@
+global.namebot = 'Rocky Bot'
+global.vs = '2.0.0'
+global.prefix = '.'
+global.libreria = 'Baileys Multi Device'
+
+global.dev = 'Moises'
+global.etiqueta = '𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔'
+
+// JIDs completos ya que aún falta hacer que pase de jid a JIDS
+global.owner = [
+  ['5214183357841', 'Fernando', true],
+  ['593998391403', 'Moises', true]
+]
+
+// Base de datos temporal
+global.db = { data: { users: {} } }
+global.plugins = {}
+
+console.log('✅ Configuración global cargada')
